ed75260cfc8f76990c207b3b76bb1e53165a81ae
sanchitpasricha <sanchit0229@gmail.com>
2024-07-11 02:30:29 +0530

    added auth middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,34 @@
 import { Hono } from "hono";
 import { userSignin, userSignup } from "./controllers/userController";
 import { displayBlogs, findBlog, postBlog } from "./controllers/blogController";
+import { verify } from "hono/jwt";
+import { Context } from "hono/jsx";
 
 const app = new Hono<{
   Bindings: {
     DATABASE_URL: string;
+    SECRET: string;
+  };
+  Variables: {
+    userId: any;
   };
 }>().basePath("/api/v1");
+
+async function authMiddleware(c: any, next: any) {
+  const token = c.req.header("Authorization")?.split(" ")[1];
+  if (!token) {
+    return c.json({ message: "Unauthorized" });
+  }
+  const payload = await verify(token, c.env.SECRET);
+  if (!payload) {
+    return c.json({ message: "Invalid token" });
+  }
+  c.set("userId", payload.userId);
+
+  next();
+}
+
+app.use("/blog/*", authMiddleware);
 
 app.post("/user/signup", userSignup);
 app.post("/user/signin", userSignin);
