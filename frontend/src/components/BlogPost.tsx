5d67e60d359f80a5003bae3753d8d65d0e9d4de6
sanchitpasricha <sanchit0229@gmail.com>
2024-07-13 16:09:37 +0530

    Blog Post Component

--- frontend/src/components/BlogPost.tsx
+++ frontend/src/components/BlogPost.tsx
@@ -2,9 +2,28 @@ import { Blog } from "../interfaces/BlogInterface";
 
 function BlogPost({ blog }: { blog: Blog }) {
   return (
-    <div>
-      <h1>{blog.title}</h1>
-      <h2>{blog.content}</h2>
+    <div className="max-w-xl mx-auto p-4 bg-white rounded-lg shadow-md">
+      <div className="flex items-center mb-4">
+        <img
+          src={
+            "https://media.sproutsocial.com/uploads/2019/09/how-to-write-a-blog-post.svg"
+          }
+          alt="Post"
+          className="w-16 h-16 object-cover"
+        />
+        <div className="ml-4">
+          <h2 className="text-xl font-bold">{blog.title}</h2>
+          <p className="text-gray-600">{blog.content}</p>
+          <div className="text-gray-500 text-sm"></div>
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <button className="text-yellow-500">⭐</button>
+        <button className="text-gray-500">👏</button>
+        <button className="text-gray-500">➖</button>
+        <button className="text-gray-500">🔖</button>
+        <button className="text-gray-500">•••</button>
+      </div>
     </div>
   );
 }
