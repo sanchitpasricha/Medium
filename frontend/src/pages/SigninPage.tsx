8121953348dbdcc2be58e67eca7b7f96f47adb48
sanchitpasricha <sanchit0229@gmail.com>
2024-07-14 20:26:05 +0530

    Signin form with UI and backend call

--- frontend/src/pages/SigninPage.tsx
+++ frontend/src/pages/SigninPage.tsx
@@ -1,5 +1,97 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 function SigninPage() {
-  return <div>Signin Page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  async function SubmitForm() {
+    const response = await axios.post(
+      "https://backend.sanchit0229.workers.dev/api/v1/user/signin",
+      {
+        email: email,
+        password: password,
+      }
+    );
+    setEmail("");
+    setPassword("");
+    if (response.data.token) {
+      alert("Login successful");
+      navigate("/blogs");
+    }
+    localStorage.setItem("token", response.data.token);
+  }
+
+  return (
+    <div className="flex h-screen  flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <div>
+          <label className="block text-sm font-medium leading-6 text-gray-900">
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              type="email"
+              required
+              className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
+            <label className="block my-2 text-sm font-medium leading-6 text-gray-900">
+              Password
+            </label>
+          </div>
+          <div className="mt-2">
+            <input
+              type="password"
+              required
+              className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            onClick={SubmitForm}
+            type="submit"
+            className="flex my-6 w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not a member?{" "}
+          <Link
+            to={"/"}
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Signup
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default SigninPage;
