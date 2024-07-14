1900a5070649fb4c8f37babda8ad4cabbecea561
sanchitpasricha <sanchit0229@gmail.com>
2024-07-14 19:34:18 +0530

    signup axios to backend, jwt in local storage

--- frontend/src/components/SignupForm.tsx
+++ frontend/src/components/SignupForm.tsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import axios from "axios";
+
 function SignupForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function SubmitForm() {
+    const response = await axios.post(
+      "https://backend.sanchit0229.workers.dev/api/v1/user/signup",
+      {
+        email: email,
+        password: password,
+        name: name,
+      }
+    );
+    setName("");
+    setEmail("");
+    setPassword("");
+    localStorage.setItem("token", response.data.token);
+  }
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm ">
@@ -17,62 +39,60 @@ function SignupForm() {
           </label>
           <div className="mt-2">
             <input
-              id="email"
-              name="email"
               type="email"
               required
-              autoComplete="email"
               className="p-2 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              value={name}
             />
           </div>
         </div>
 
         <div>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
+          <label className="block text-sm font-medium leading-6 text-gray-900">
             Email address
           </label>
           <div className="mt-2">
             <input
-              id="email"
-              name="email"
               type="email"
               required
-              autoComplete="email"
               className="block w-full rounded-md border-0 p-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
             />
           </div>
         </div>
 
         <div>
           <div className="flex items-center justify-between">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
+            <label className="block text-sm font-medium leading-6 text-gray-900">
               Password
             </label>
           </div>
           <div className="mt-2">
             <input
-              id="password"
-              name="password"
               type="password"
               required
-              autoComplete="current-password"
               className="p-2 block w-full rounded-md text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
             />
           </div>
         </div>
 
         <div>
           <button
+            onClick={SubmitForm}
             type="submit"
             className="flex w-full my-6 justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
           >
-            Sign in
+            Sign up
           </button>
         </div>
       </div>
